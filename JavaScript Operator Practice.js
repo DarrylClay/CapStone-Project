5a0fb0fc25fcc981a37ab664d9c7658673ce218e
DarrylClay <darryljclay14@gmail.com>
2024-10-04 22:43:14 -0500

    Add files via upload

---
+++ JavaScript Operator Practice.js
@@ -0,0 +1,21 @@
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+const coder = "your name";
+const languages = ["JavaScript", "HTML"];
+languages.push("CSS");
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+let text4 = "John";
+let text5 = "Doe";
+let text3 = text1 + " " + text2;
+
+
+
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
